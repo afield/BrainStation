2a83cbd0c8934d1c9b50e205bac85a414744ec8f
afield <afield@brainstation.io>
2016-01-25 10:17:04 -0500

    first

--- WDI-Git/Assignments/Chapter 3 - Controllers/Objects/Car/js/app.js
+++ WDI-Git/Assignments/Chapter 3 - Controllers/Objects/Car/js/app.js
@@ -1,60 +1,126 @@
-
-    var make = $('#make');
-    var model = $('#model');
-    var colour = $('#colour');
-    var amount = $('#amount');
-
-function CarFactory(make,model,colour){
-	this.make = make;
-	this.model = model;
-	this.colour = colour;
-	this.cars = [];
-}
-
-CarFactory.prototype.manufacture = function(num){
-	for(var i = 0; i < num; i++){
-		
-		this.cars.push(new Car(this.make,this.model,this.colour));
-	}
-}
-
-CarFactory.prototype.showCars = function(){
-	for(var i = 0; i < this.cars.length; i++){
-		console.log(this.cars[i]);
-	}
-}
-function Car(make,model,colour){
-	this.make = make;
-	this.model = model;
-	this.colour = colour;
-}
-
-
-    //Populate table
-function populateTable(){
-
-    $("#car-table").find('tbody')
-    .append($('<tr>')
-        .append($('<td>')
-                .text(make.val())      
-        ).append($('<td>')
-                .text(model.val())
-        ).append($('<td>')
-                .text(colour.val())
-        ).append($('<td>')
-                .text(amount.val())
-        )
-            
-    );
-}
-
-$('#create-form').submit(function(event){
-    event.preventDefault();
-    factory = new CarFactory(this.make,this.model,this.colour);
-    factory.manufacture(amount.val());     
-    populateTable();
-    console.log(factory.cars.length);
+/**Pseudocode - IMPORTANT!! - Keep up with the pseudocode guys!
+    This program will have a form filled with user input fields - make, model, colour, amount
+    When a user submits the form correctly
+    All of the user inputs will be collected and saved as variables in a Car Object
+    A function will take the user amount input and create that many Car Objects
+    These objects will be displayed on a table in the correct sections
+    A user will then be able to go back and forth, populating the table based on the user inputs
     
+    --Amanda Field -- Jan 21, 2016--
+*/
 
+//Global Variables
+var make = $('#make');
+var model = $('#model');
+var colour = $('#colour');
+var amount = $('#amount');
+
+//Car Factory Object
+function CarFactory(make, model, colour) {
+    this.make = make;
+    this.model = model;
+    this.colour = colour;
+    this.cars = [];
+}
+
+//Iterate through car objects based on users amount input
+CarFactory.prototype.manufacture = function (num) {
+    for (var i = 0; i < num; i++) {
+        this.cars.push(new Car(make.val(), model.val(), colour.val()));
+    }
+}
+
+//Used to display how many objects are created - will iterate through each object and log on console
+CarFactory.prototype.showCars = function () {
+    for (var i = 0; i < this.cars.length; i++) {
+        console.log(this.cars[i]);
+    }
+}
+
+//Constructor for Car Object
+function Car(make, model, colour) {
+    this.make = make;
+    this.model = model;
+    this.colour = colour;
+}
+
+
+//Populate table
+function populateTable() {
+    //Insert all the values inputted by the user
+    $("#car-table").find('tbody')
+        //Append to add new element
+        .append($('<tr>')
+            //td element
+            .append($('<td>')
+                //text inputs content
+                .text(make.val())
+            )
+            .append($('<td>')
+                .text(model.val())
+            )
+            .append($('<td>')
+                .text(colour.val())
+            )
+            .append($('<td>')
+                .text(amount.val())
+            )
+
+        );
+}
+
+//When Submit is selected
+$('#create-form').submit(function (event) {
+    event.preventDefault();
+
+    //If the user input is a number (not - not a number)
+    if (!(isNaN(amount.val()))) {
+        
+        //create new Factory object
+        factory = new CarFactory(this.make, this.model, this.colour);
+        
+        //Pass amount in through manufacture function
+        factory.manufacture(amount.val());
+        
+        //Populate the table based on values user had inputted
+        populateTable();
+        
+        //change css properties
+        $('#create-container').css('display', 'none');
+        $('#view-container').css('display', 'block');
+       
+        //log car objects created in the console 
+        factory.showCars();
+
+        //When the back button is selected
+        $('#back-button').click(function () {
+            //show create container
+            $('#create-container').css('display', 'block');
+            //hide display container
+            $('#view-container').css('display', 'none');
+        });
+        
+        //call my new clearInputs function
+        clearInputs();
+
+    } else {
+        //Error handling - If amount user input is NaN
+        alert("Please enter a numeric value in the amount field");
+    }
 });
 
+
+/*SIDE NOTE -- Suzanne added an idea to erase the data every time
+you click back in the creat form */ 
+//This would simply require a quick function that we will call after
+//we click submit
+
+/*Copied and pasted this from the first result of googling
+"jQuery erase form data" - just changed the values and VOILA*/
+function clearInputs(){
+    $("#make").val('');
+    $("#model").val('');
+    $("#colour").val('');
+    $("#amount").val('');
+    //:)
+}
---
+++ WDI-Git/Assignments/Chapter 3 - Controllers/Objects/Journal/Basic_Journal/app.js
@@ -0,0 +1,78 @@
+
+
+$(document).ready(function () {
+	//this is triggered by the button "submitting" the form
+	$(".theform").submit(function (event) {
+		event.preventDefault();
+		//make sure the above is included so your form doesnt refresh your page!
+
+		var frm = $(".theform");
+		//grabs form and stores it in a variable
+
+		myJournal.addEntry(frm);
+		myJournal.displayEntries();
+	});
+
+});
+
+function Entry (title, author, content) {
+	this.title = title;
+	this.author = author;
+	this.content = content;
+}
+
+//classes or constructor functions
+function Journal(){
+	this.entries = [];
+}
+//prototypes
+
+Journal.prototype.addEntry = function(frm) {
+	//grabs the input values from each form inpout field
+	var title = frm.find("input[name='title']").val();
+	var author = frm.find("input[name='author']").val();
+	var content = frm.find("textarea[name='content']").val();
+
+	// makes new entry with inputs
+	var entry = new Entry(title, author, content);
+
+	//adds entry to our Journal entries array
+	this.entries.unshift(entry);
+
+};
+
+
+Journal.prototype.displayEntries = function() {
+	
+	var n = this.entries.length;
+	var html = "";
+
+	// this loop loops over the array and using "i" to access the index of thre array
+	for(var i = 0; i < n ;i++){
+
+		//check if the array index is even or odd and then adds a different style
+		if(i%2){
+
+			html+="<section>";
+			html+=	"<div class='eachEntry2'>"
+
+		} else {
+			html+="<section>";
+			html+=	"<div class='eachEntry'>"
+		}
+
+		//adds all of the html to a string
+		html += "<h2> Title: " + this.entries[i].title + "</h2>";
+		html += "<h3> Author:" + this.entries[i].author + "</h3>";
+		html += "<p>" + this.entries[i].content + "</p>";
+		html += "</div></section><hr>"
+
+	}
+	//injects our string html into our index file at theJournal class element
+	$(".theJournal").html(html);
+};
+
+
+//variables
+var myJournal = new Journal();
+
