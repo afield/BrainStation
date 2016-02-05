951027e11e0a5e3f4b040d21ef90627fbb8549ae
afield <afield@brainstation.io>
2016-02-05 09:37:06 -0500

    morning

---
+++ WDI/Assignments/Chapter 5 - Angular/angularForms/js/app.js
@@ -0,0 +1,27 @@
+
+var app = angular.module('myModule', ['ui.bootstrap']);
+app.controller('PopoverDemoCtrl', function ($scope) {
+  $scope.dynamicPopover = {
+    content: 'Hello, World!',
+    templateUrl: 'index.html',
+    title: 'Title'
+  };
+
+  $scope.placement = {
+    options: [
+      'top',
+      'top-left',
+      'top-right',
+      'bottom',
+      'bottom-left',
+      'bottom-right',
+      'left',
+      'left-top',
+      'left-bottom',
+      'right',
+      'right-top',
+      'right-bottom'
+    ],
+    selected: 'top'
+  };
+});
